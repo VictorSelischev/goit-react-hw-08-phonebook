4f9c7a3407df4d6106cb4e9cd6a51605379c8246
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-06 11:50:12 +0200

    change baseURL  API

--- src/services/operations.js
+++ src/services/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://639e9a8f3542a261305e4d50.mockapi.io/api/v1';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
