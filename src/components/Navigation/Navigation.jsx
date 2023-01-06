30223633ba7bb07907c709cde58ce00fc5621aa0
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-06 20:31:50 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,6 +4,7 @@ import css from './Navigation.module.css';
 export const Navigation = () => {
     return (
         <nav>
+            <NavLink to='/'>Home</NavLink>
             <NavLink to="/register">Register</NavLink>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/contacts">Contacts</NavLink>
