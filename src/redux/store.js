5e03fe67512e4af21d6eafca7511716ece3de39b
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-07 14:29:06 +0200

    summary logic

---
+++ src/redux/store.js
@@ -0,0 +1,44 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+// WHITELIST
+const persistConfig = {
+  key: 'auth',
+  storage: storage,
+  whitelist: ['token'], // only navigation will be persisted
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
