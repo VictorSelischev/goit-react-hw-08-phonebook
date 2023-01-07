25db95881ed431f0a8d750fdb4d1a4fda4dfe6ef
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-07 12:40:57 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,30 @@
+import { useDispatch } from "react-redux";
+
 const Register = () => {
-    return ();
-}
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch();
+        const form = e.currentTarget;
+        form.reset();
+    };
+
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit} >
+      <label>
+        <input type="text" name="name" />
+      </label>
+      <label>
+        <input type="email" name="email" />
+      </label>
+      <label>
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
 
 export default Register;
