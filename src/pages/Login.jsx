b378e2eef7bb5ee36ae994075500ac9b8e45ac83
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-07 12:41:04 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,29 @@
+import { useDispatch } from "react-redux";
+
 const Login = () => {
-    return ();
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        dispatch();
+        form.reset();
+    };
+
+    return (
+        <div>
+            <form autoComplete="off" onSubmit={handleSubmit} >
+                <label>
+                    <input type="email" name="email" />
+                </label>
+                <label>
+                    <input type="password" name="password" />
+                </label>
+                <button type="submit" ></button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
