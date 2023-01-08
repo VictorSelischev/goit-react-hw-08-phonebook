2085a3379ab8e438a01b9b7fa1857257b0138859
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-08 18:09:18 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,33 @@
 const styles = {
-    container: {
-        minHeight: 'calc(100vh - 50px)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontWeight: 500,
-        fontSize: 32,
-        textAlign: 'center',
-    },
-    paragraph: {
-        fontWeight: 500,
-        fontSize: 16,
-        textAlign: 'center',
-    }
-}
+  container: {
+    // minHeight: 'calc(100vh - 50px)',
+      marginTop: 80,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 700,
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  paragraph: {
+    fontWeight: 500,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+};
 
 const Home = () => {
-    return (
-        <div style={styles.container}>
-            <h1 style={styles.title} >Hello friend!</h1>
-            <p style={styles.paragraph} >Please register and login in your contacts list</p>
-        </div>
-    );
-}
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Hello friend!</h1>
+      <p style={styles.paragraph}>
+        Please register and login to go to your contacts list
+      </p>
+    </div>
+  );
+};
 
 export default Home;
