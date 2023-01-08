0f9fb5c67a63af486c674d64389c03ecb81de2c6
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-08 12:09:09 +0200

    create login & logOut operations

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -3,11 +3,37 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
-export const register = createAsyncThunk('auth/register', async (credentials, thunkAPI) => {
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
     try {
-        const response = await axios.post('/users/signup', credentials);
-        return response.data
+      const response = await axios.post('/users/signup', credentials);
+      return response.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/login', credentials);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    const response = await axios.post('/users/logout');
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });
+
+export const getContact = createAsyncThunk();
