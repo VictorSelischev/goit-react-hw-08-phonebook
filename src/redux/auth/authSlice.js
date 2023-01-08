e3476a4408bb0facddb9456b6cc272157d6a1000
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-08 12:35:12 +0200

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,15 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { register, login, logOut, getContact } from './authOperation';
 
 const authInitialState = {
-    user: {name: null, email: null},
-    token: null,
-    isLoggedIn: false,
-}
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+};
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState: authInitialState,
-    extraReducers: {},
+  name: 'auth',
+  initialState: authInitialState,
+  extraReducers: {
+    [register.fulfilled](state, action) {},
+  },
 });
 
 export const authReducer = authSlice.reducer;
