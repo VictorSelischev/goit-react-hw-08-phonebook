8dec81ad6a921ec4bc81369d7cbb960fa706de3e
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-08 23:04:39 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,24 +1,22 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectIsLoading, selectError } from "redux/selectors";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectIsLoading, selectError } from 'redux/selectors';
+import { useEffect } from 'react';
 
-import { Section } from "components/Section/Section";
-import { ContactForm } from "components/ContactForm/ContactForm";
-import { Filter } from "components/Filter/Filter";
-import { ContactsList } from "components/Contacts/ContactsList";
+import { Section } from 'components/Section/Section';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactsList } from 'components/Contacts/ContactsList';
 
-import { fetchContacts } from "services/operations";
-
+import { fetchContacts } from 'services/operations';
 
 const Contacts = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-      const isLoading = useSelector(selectIsLoading);
+  const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
 
   return (
