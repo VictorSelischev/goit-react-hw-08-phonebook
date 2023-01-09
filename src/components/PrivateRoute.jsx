7745f7ab5153669b7b7e6f57bf9470dcc3935011
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-09 20:34:45 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,5 +3,5 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({redirectTo = '/', component: Component }) => {
     const { isLoggedIn } = useAuth();
-    return isLoggedIn ? <Navigate to={ redirectTo} /> : Component ;
+    return isLoggedIn ? Component : <Navigate to={ redirectTo} /> ;
 }
